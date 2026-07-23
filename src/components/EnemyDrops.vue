ae04fabb1576065b4c44fe5eb062779374b9dd70
TheEgglet <woodstuffthings@gmail.com>
2026-07-23 11:39:44 +0200

    add animation for enemy drops

--- src/components/EnemyDrops.vue
+++ src/components/EnemyDrops.vue
@@ -1,13 +1,53 @@
 <script setup lang="ts">
 import { useEnemyStore } from '@/stores/enemy';
+import gsap from 'gsap';
 import { storeToRefs } from 'pinia';
+import { ref, watch } from 'vue';
+
+const el = ref<HTMLElement>();
 
 const enemyStore = useEnemyStore();
-const { goldDrop } = storeToRefs(enemyStore);
+const { number } = storeToRefs(enemyStore);
+
+const goldDrop = ref(0);
+
+function fetchEnemyInfo() {
+	goldDrop.value = enemyStore.goldDrop
+}
+
+function pop() {
+	if (!el.value) return;
+
+	let tl = gsap.timeline();
+
+	tl.to(el.value, {
+		opacity: 0,
+		scale: 1.3,
+
+		duration: 0.18,
+		ease: "power1.out",
+
+		onComplete: fetchEnemyInfo,
+	});
+	tl.set(el.value, {
+		scale: 1,
+	});
+	tl.to(el.value, {
+		opacity: 1,
+		scale: 1,
+
+		duration: 0.4,
+		delay: 0.2,
+		ease: "power1.inOut",
+	});
+}
+
+fetchEnemyInfo();
+watch(number, pop);
 </script>
 
 <template>
-	<div class="enemy-drops">
+	<div class="enemy-drops" ref="el">
 		{{ goldDrop }}🪙
 	</div>
 </template>
@@ -20,7 +60,7 @@ const { goldDrop } = storeToRefs(enemyStore);
 
 	white-space: nowrap;
 
-	background-color: #0003;
+	background-color: #0002;
 	border-radius: 4px;
 	padding: 2px 4px 2px 6px;
 	font-size: 0.8em;
