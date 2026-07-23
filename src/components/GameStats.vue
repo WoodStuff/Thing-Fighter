1b256c4ca3ab25baaa0aa7940d0ff5228146fde2
TheEgglet <woodstuffthings@gmail.com>
2026-07-23 12:02:48 +0200

    animate gold counter

--- src/components/GameStats.vue
+++ src/components/GameStats.vue
@@ -1,9 +1,21 @@
 <script setup lang="ts">
 import { useStatsStore } from '@/stores/stats';
+import gsap from 'gsap';
 import { storeToRefs } from 'pinia';
+import { computed, reactive, watch } from 'vue';
 
 const stats = useStatsStore();
-const { gold, kills, deaths } = storeToRefs(stats);
+const { kills, deaths } = storeToRefs(stats);
+
+const tweened = reactive({
+	gold: stats.gold,
+});
+
+const gold = computed(() => tweened.gold.toFixed(0));
+
+watch(() => stats.gold, n => {
+	gsap.to(tweened, { gold: n, duration: 0.5 });
+})
 </script>
 
 <template>
